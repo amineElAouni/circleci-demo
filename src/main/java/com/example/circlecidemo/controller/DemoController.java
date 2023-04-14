143d02aa8cf3885b7332b8f5193b71f438fd129a
Amine <elaouni.mohamedamine@gmail.com>
2023-04-14 22:16:11 +0000

    first commit

---
+++ src/main/java/com/example/circlecidemo/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.example.circlecidemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World !!!!!";
+    }
+}
